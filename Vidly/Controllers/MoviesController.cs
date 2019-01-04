3a4da75aeea4d3099a9474306b583a1e6407586e
Martin Georgiev Georgiev <mggeorgiev@gmail.com>
2019-01-04 09:48:20 +0200

    ActionParameters

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -15,5 +15,14 @@ namespace Vidly.Controllers
             var movie = new Movie() { Name = "Shrek!" };
             return View(movie);
         }
+
+        public ActionResult Index (int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name"; 
+            return Content(String.Format("pageIndex ={0}&sortBy={1}",pageIndex,sortBy));
+        }
     }
 }
